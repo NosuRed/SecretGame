9043fab12dddde2775238cd074d0197cbb0b0e33
NosuRed <behrensphilipp1@gmail.com>
2019-05-13 00:45:12 +0200

    Help

--- Area2D - Copy.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-export(String, FILE, "*.tscn") var NextWorld
-
-func _physics_process(delta):
-	var bodies = get_overlapping_bodies()
-	for body in bodies:
-		if body.name == "Player":
-			print("Dead")
